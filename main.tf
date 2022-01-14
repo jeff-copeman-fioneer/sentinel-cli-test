1691aec14acd00aa717e7ecedf3919e301760a54
Jeff Copeman <jeff.copeman@sap.com>
2022-01-14 14:08:07 -0500

    Adding terraform and sentinel files

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  cloud {
+    organization = "Fioneer"
+	workspaces {
+      name = "learn-terraform-azure"
+	}
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
